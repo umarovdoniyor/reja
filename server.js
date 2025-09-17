b08aa45a9d68c7826067342c94b9fb0248d8a283
umarovdoniyor <mr.danny951008@gmail.com>
2025-09-18 02:39:56 +0900

    feat: develop author router page

--- server.js
+++ server.js
@@ -2,6 +2,16 @@ console.log('Web server');
 const express = require('express');
 const app = express();
 const http = require('http');
+const fs = require('fs');
+
+let user;
+fs.readFile('database/user.json', 'utf8', (err, data) => {
+  if (err) {
+    console.log('ERROR: ', err);
+  } else {
+    user = JSON.parse(data);
+  }
+});
 
 // 1: Kirish code
 app.use(express.static('public'));
@@ -15,9 +25,13 @@ app.set('views', 'views');
 app.set('view engine', 'ejs');
 
 // 4: Routing code
-app.post('/create-item', (req, res) => {
-  console.log(req.body);
-  res.json({ test: 'success' });
+// app.post('/create-item', (req, res) => {
+//   console.log(req.body);
+//   res.json({ test: 'success' });
+// });
+
+app.get('/author', (req, res) => {
+  res.render('author', { user: user });
 });
 
 app.get('/', function (req, res) {
---
+++ views/author.ejs
@@ -0,0 +1,1 @@
+<h1>Authors page</h1>
